1d26749592faa534cfbe31e170c59fae1bad4839
Andrey Kashapov <kashapovandrey@gmail.com>
2018-02-22 21:14:58 +0300

    sorter v 0.4

--- src/index.js
+++ src/index.js
@@ -21,26 +21,16 @@ class Sorter {
   get length() {
     //return the count of current elements, which were added to Sorter instance via add method
     return this.arr.length;
-    //return 1;
   }
 
   toArray() {
-    //return all elements in array
-    //return '[ ' + this.arr.join(', ') + ' ]';
+    //return all elements in array    
     return this.arr;
   }
 
   sort(indices) {
     //takes indices of already added elements and sorts only these elements
-    /*function compareNumeric(a, b) {
-      if (a > b) return 1;
-      if (a < b) return -1;
-    };*/
-
-    //return this.arr.sort(this.setComparator(indices));
-  
-
-    //indices.sort(setComparator);
+    
     return this.arr.sort();
   }
 
